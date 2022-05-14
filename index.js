3c957a91e35b7a33d5d29eec2a81cd7704eeafe9
nitishk754 <nitishk@instaacoders.com>
2022-05-14 16:47:55 +0530

    Post request form handling issue resolved using body-parser

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const express = require('express');
-const res = require('express/lib/response');
-const bodyParser = require('body-parser')
+const bodyParser= require('body-parser')
 const port = 3000
 const app= express();
 
 // include router module
 const router = require('./routes/router')
 app.use('/',router)
-app.use(bodyParser.urlencoded({ extended:false}))
+// create application/json parser
+var jsonParser = bodyParser.json()
+// app.use(bodyParser.urlencoded({ extended: true }));
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // set templating engine and views path 
 
 app.set('view engine','ejs')
-app.set('views','./views')
+// app.set('views','./views')
 
 // to parse json data 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 // define static path 
 app.use(express.static('public'))
+
+
+
+
 // start server
 app.listen(port, function(){
     console.log("Server running on Port " + port )
